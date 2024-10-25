81bf029242e51aa589535e4de6d407f0015be6d3
Pratik Bhattacharjee <pratikbhattacharjee09@gmail.com>
2024-10-25 16:16:52 +0800

    Routes set up

---
+++ app/_layout.jsx
@@ -0,0 +1,14 @@
+import { Slot, Stack } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
+
+import React from "react";
+
+const RootLayout = () => {
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+    </Stack>
+  );
+};
+
+export default RootLayout;
